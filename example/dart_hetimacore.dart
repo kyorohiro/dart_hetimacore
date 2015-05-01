b5d73bfe4102cb734beabd5310eea3c4525ef058
kyorohiro <kyorohiro@gmail.com>
2015-05-01 22:58:13 +0900

    first commit

---
+++ example/dart_hetimacore.dart
@@ -0,0 +1,11 @@
+// Copyright (c) 2015, <your name>. All rights reserved. Use of this source code
+// is governed by a BSD-style license that can be found in the LICENSE file.
+
+library dart_hetimacore.example;
+
+import 'package:hetimacore/hetimacore.dart';
+
+main() {
+  var awesome = new Awesome();
+  print('awesome: ${awesome.isAwesome}');
+}
