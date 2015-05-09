8f1c46bea08e875428a73a769a847023bf4f0dc8
kyorohiro <kyorohiro@gmail.com>
2015-05-10 01:52:15 +0900

    add test

---
+++ test/all_test_b.dart
@@ -0,0 +1,13 @@
+// Copyright (c) 2015, <your name>. All rights reserved. Use of this source code
+// is governed by a BSD-style license that can be found in the LICENSE file.
+
+library dart_hetimacore.test.b;
+
+import 'package:unittest/unittest.dart';
+import 'package:hetimacore/hetimacore.dart';
+import 'arraybuilder_test.dart' as arraybuilder00;
+
+main() {
+  arraybuilder00.main();
+}
+
