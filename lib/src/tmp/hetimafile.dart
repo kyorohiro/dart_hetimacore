3bf2693ffd3c93c2819dac35318962576017a430
kyorohiro <kyorohiro@gmail.com>
2015-06-02 01:47:24 +0900

    ..

--- lib/src/tmp/hetimafile.dart
+++ lib/src/tmp/hetimafile.dart
@@ -1,10 +1,7 @@
 library hetimacore.file;
 import 'dart:typed_data' as data;
-import 'dart:math' as math;
-import 'dart:convert' as convert;
 import 'dart:async' as async;
 import 'dart:core';
-import 'dart:html' as html;
 import '../../hetimacore.dart';
 
 
