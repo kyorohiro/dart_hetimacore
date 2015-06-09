e6486d431b74118a2f1d63f1a03d0a3ce4e0cf3b
kyorohiro <kyorohiro@gmail.com>
2015-06-09 23:26:33 +0900

    ..

--- test/hetimadata_cache_test.dart
+++ test/hetimadata_cache_test.dart
@@ -86,5 +86,20 @@ void main() {
     });
 
     //
+    unit.test("hetimadata_cache 6: init", () {
+      HetimaDataMemory dummy = new HetimaDataMemory();
+      HetimaDataCache cache = new HetimaDataCache(dummy,cacheSize: 3, cacheNum: 3);
+      return cache.write([1,2,3,4,5,6,7], 4).then((WriteResult w) {
+        return cache.read(0, 11);
+      }).then((ReadResult r) {
+        print("--1--");
+        unit.expect(r.buffer, [0,0,0,0,1,2,3,4,5,6,7]);
+        return cache.flush();
+      }).then((_) {
+        print("--2--");
+        unit.expect(dummy.getBuffer(0, 100), [0,0,0,0,1,2,3,4,5,6,7]);
+     });
+    });
+    //
   });
 }
